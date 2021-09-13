3488871f4e8fdf0456ada79785ce6a07cd85301f
Phuc Le <phuclevinh2000@gmail.com>
2021-09-13 19:36:35 +0300

    Mongo add file into database and console log out

--- part3/mongo.js
+++ part3/mongo.js
@@ -5,10 +5,10 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.argv[2]  //run mongoose with cmd node mongo.js password (password is the password, not password)
 
 const url =
-  `mongodb+srv://fullstack:Wo123456@cluster0.bg6jr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.bg6jr.mongodb.net/note-app?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
@@ -20,13 +20,20 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
+// const note = new Note({
+//   content: 'HTML is Easy lam',
+//   date: new Date(),
+//   important: true,
+// })
 
-note.save().then(result => {
-  console.log('note saved!')
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
